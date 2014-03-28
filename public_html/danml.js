266ea0a46d08df8dcb162f8f46cb9880d9933f3b
Oshanz <spiritualmirror@gmail.com>
2014-03-28 13:51:49 +0530

    chmod

---
+++ public_html/danml.js
@@ -0,0 +1,52 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function download(strData, strFileName, strMimeType) {
+    var D = document,
+            a = D.createElement("a");
+    strMimeType = strMimeType || "application/octet-stream";
+
+
+
+    if (window.MSBlobBuilder) { //IE10+ routine
+        var bb = new MSBlobBuilder();
+        bb.append(strData);
+        return navigator.msSaveBlob(bb, strFileName);
+    } /* end if(window.MSBlobBuilder) */
+
+
+    if ('download' in a) { //html5 A[download]
+        a.href = "data:" + strMimeType + "," + encodeURIComponent(strData);
+        a.setAttribute("download", strFileName);
+        a.innerHTML = "downloading...";
+        D.body.appendChild(a);
+        setTimeout(function() {
+            a.click();
+            D.body.removeChild(a);
+        }, 66);
+        return true;
+    } /* end if('download' in a) */
+
+
+    //do iframe dataURL download (old ch+FF):
+    var f = D.createElement("iframe");
+    D.body.appendChild(f);
+    f.src = "data:" + strMimeType + "," + encodeURIComponent(strData);
+
+    setTimeout(function() {
+        D.body.removeChild(f);
+    }, 333);
+    return true;
+}
+
+function ExportExcel(table) {
+    if (!table.nodeType) {
+        table = document.getElementById(table);
+        download(table.outerHTML, "ftable.xls", "application/vnd.ms-excel");
+    }else{
+        alert('not a tbl');
+    }
+}
