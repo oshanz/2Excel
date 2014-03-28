05c0c46449e51718037e81854d16c94e30785653
Oshanz <spiritualmirror@gmail.com>
2014-03-28 16:15:05 +0530

    library cleaned

--- public_html/danml.js
+++ public_html/danml.js
@@ -4,49 +4,35 @@
  */
 
 
-function download(strData, strFileName, strMimeType) {
-    var D = document,
-            a = D.createElement("a");
-    strMimeType = strMimeType || "application/octet-stream";
+function download(strData, strFileName) {
 
-
-
-    if (window.MSBlobBuilder) { //IE10+ routine
-        var bb = new MSBlobBuilder();
-        bb.append(strData);
-        return navigator.msSaveBlob(bb, strFileName);
-    } /* end if(window.MSBlobBuilder) */
-
-
-    if ('download' in a) { //html5 A[download]
-        a.href = "data:" + strMimeType + "," + encodeURIComponent(strData);
-        a.setAttribute("download", strFileName);
-        a.innerHTML = "downloading...";
-        D.body.appendChild(a);
-        setTimeout(function() {
-            a.click();
-            D.body.removeChild(a);
-        }, 66);
-        return true;
-    } /* end if('download' in a) */
-
-
-    //do iframe dataURL download (old ch+FF):
-    var f = D.createElement("iframe");
-    D.body.appendChild(f);
-    f.src = "data:" + strMimeType + "," + encodeURIComponent(strData);
-
-    setTimeout(function() {
-        D.body.removeChild(f);
-    }, 333);
-    return true;
 }
 
 function ExportExcel(table) {
     if (!table.nodeType) {
-        table = document.getElementById(table);
-        download(table.outerHTML, "ftable.xls", "application/vnd.ms-excel");
-    }else{
-        alert('not a tbl');
+        var strData = document.getElementById(table).outerHTML;
+        var strFileName = "ftable.xls";
+        var D = document, a = D.createElement("a");
+        var strMimeType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+
+        if ('download' in a) { //html5 A[download]
+            a.href = "data:" + strMimeType + "," + encodeURIComponent(strData);
+            a.setAttribute("download", strFileName);
+            a.innerHTML = "downloading...";
+            D.body.appendChild(a);
+            setTimeout(function() {
+                a.click();
+                D.body.removeChild(a);
+            }, 66);
+            return true;
+        } /* end if('download' in a) */
+
+        setTimeout(function() {
+            D.body.removeChild(f);
+        }, 333);
+        return true;
+    } else {
+        alert('Not a table');
+        return false;
     }
 }
