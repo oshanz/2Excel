575c3ab2866a3e111bc083fb4f522b8b5c228c26
Oshanz <spiritualmirror@gmail.com>
2014-03-31 19:58:10 +0530

    wow i am awsome

--- public_html/ExportExcel.js
+++ public_html/ExportExcel.js
@@ -43,28 +43,13 @@ function getHeaders() {
 }
 
 function count() {
-	var div_sort_out = document.createElement('div');
-	//    div_sort_out.setAttribute('hidden', 'true');
-	div_sort_out.setAttribute('id', 'div_sort_out');
-	var div_sort = ['<div><ul id="sortable">'];
-	$.each($.find("input[name=type]:checked"), function(index, v) {
-		div_sort.push('<li class="ui-state-default">' + v.value + '</li>');
-	});
-	div_sort.push('</ul><div>');
-	div_sort_out.innerHTML = div_sort.join('');
-	document.body.appendChild(div_sort_out);
-	$("#sortable").sortable();
-	$("#sortable").disableSelection();
-}
-
-function create() {
 	var all = [];
 	$.each(document.getElementById('tblId').rows[0].cells, function(index, v) {
 		all.push(index);
 	});
 	var select = [];
-	$.each($.find("#sortable li"), function(index, v) {
-		select.push(parseInt(v.innerHTML.trim()));
+	$.each($.find("input[name=type]:checked"), function(index, v) {
+		select.push(parseInt(v.value));
 	});
 	var rem = $(all).not(select).get();
 	var htmlData = $('#tblId').clone();
@@ -72,5 +57,8 @@ function create() {
 	for (var i = 0; i < l; i++) {
 		htmlData.find("tr th:eq(" + (rem[i] - i) + "),tr td:eq(" + (rem[i] - i) + ")").remove().end().html();
 	}
-	window.open('data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,' + encodeURIComponent('<table>' + htmlData.html() + '</table>'));
+	var a = document.createElement('a');
+	a.href = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,' + encodeURIComponent('<table>' + htmlData.html() + '</table>');
+	a.setAttribute('download', 'strFileName' + '_' + new Date().toLocaleString() + '.xlsx');
+	a.click();
 }
---
+++ public_html/sort.js
@@ -0,0 +1,19 @@
+/**
+ * @author Trisquel
+ */
+
+
+function count() {
+	var div_sort_out = document.createElement('div');
+	//    div_sort_out.setAttribute('hidden', 'true');
+	div_sort_out.setAttribute('id', 'div_sort_out');
+	var div_sort = ['<div><ul id="sortable">'];
+	$.each($.find("input[name=type]:checked"), function(index, v) {
+		div_sort.push('<li class="ui-state-default">' + v.value + '</li>');
+	});
+	div_sort.push('</ul><div>');
+	div_sort_out.innerHTML = div_sort.join('');
+	document.body.appendChild(div_sort_out);
+	$("#sortable").sortable();
+	$("#sortable").disableSelection();
+}
