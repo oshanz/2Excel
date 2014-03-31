01cf7ea6ff8effb10175b30cb9cf28292e2d0b07
Oshanz <spiritualmirror@gmail.com>
2014-03-31 10:03:58 +0530

    design changed

--- public_html/ExportExcel.js
+++ public_html/ExportExcel.js
@@ -29,21 +29,40 @@ function ExportExcel(table, strFileName) {
 
 function getHeaders() {
     var div_out = document.createElement('div');
-    div_out.setAttribute('hidden', 'true');
-    var div = document.createElement('div');
-    div.setAttribute('id', 'strFileName');
-    var ol = document.createElement('ol');
-    ol.setAttribute('id', 'selectable');
-    var li;
+//    div_out.setAttribute('hidden', 'true');
+    var div_inner = ['<div id="strFileName"><table border="1" width="100%"><thead><tr><th>',
+        '<input type="button" onclick="$(' + "'#strFileName input:checkbox'" + ').prop(' + "'checked'" + ', true);" value="Select All"/>',
+        '</th><th>Column</th></tr></thead><tbody>'];
     $.each(document.getElementById('tblId').rows[0].cells, function(index, v) {
-        li = document.createElement('li');
-        li.innerHTML = v.innerHTML.trim();
-        ol.appendChild(li);
+        div_inner.push('<tr>');
+        div_inner.push('<td align="center"><input type="checkbox" value="' + index + '")/></td>');
+        div_inner.push('<td align="center">' + v.innerHTML.trim() + '</td>');
+        div_inner.push('</tr>');
     });
-    div.appendChild(ol);
-    div_out.appendChild(div);
+    div_inner.push('</tbody></div>');
+    div_out.innerHTML = div_inner.join('');
     document.body.appendChild(div_out);
     $('.inline').colorbox({inline: true, width: "50%"});
-
 //    $("#selectable").sortable();
 }
+
+//function getHeaders() {
+//    var div_out = document.createElement('div');
+//    div_out.setAttribute('hidden', 'true');
+//    var div = document.createElement('div');
+//    div.setAttribute('id', 'strFileName');
+//    var ol = document.createElement('ol');
+//    ol.setAttribute('id', 'selectable');
+//    var li;
+//    $.each(document.getElementById('tblId').rows[0].cells, function(index, v) {
+//        li = document.createElement('li');
+//        li.innerHTML = v.innerHTML.trim();
+//        ol.appendChild(li);
+//    });
+//    div.appendChild(ol);
+//    div_out.appendChild(div);
+//    document.body.appendChild(div_out);
+//    $('.inline').colorbox({inline: true, width: "50%"});
+//
+////    $("#selectable").sortable();
+//}
