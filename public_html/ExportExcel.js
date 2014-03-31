7f5c1b541079d4765499510ffd13f5db4b9fb862
Oshanz <spiritualmirror@gmail.com>
2014-03-31 07:29:32 +0530

    colorbox set headers done

--- public_html/ExportExcel.js
+++ public_html/ExportExcel.js
@@ -27,10 +27,27 @@ function ExportExcel(table, strFileName) {
 }
 
 function getHeaders() {
-    var columns = ['<div>', '<ol id="selectable">'];
+    var div_out = document.createElement('div');
+    div_out.setAttribute('hidden', 'true');
+    var div = document.createElement('div');
+    div.setAttribute('id', 'strFileName');
+    var ol = document.createElement('ol');
+    ol.setAttribute('id', 'selectable');
+    var li;
     $.each(document.getElementById('tblId').rows[0].cells, function(index, v) {
-        columns.push('<li>' + v.innerHTML.trim() + '</li>')
+        li = document.createElement('li');
+        li.innerHTML = v.innerHTML.trim();
+        ol.appendChild(li);
     });
-    columns.push('</ol>');
-    $.colorbox({html:columns.join('')});
+    div.appendChild(ol);
+    div_out.appendChild(div);
+    document.body.appendChild(div_out);
+    $('.inline').colorbox({inline: true, width: "50%"});
+    $("#selectable").selectable({
+        stop: function() {
+            $(".ui-selected", this).each(function() {
+                console.log($("#selectable li").index(this));
+            });
+        }
+    });
 }
